a7660766db64bc03fdf7d3f26c922a9831807e81
David <daviddavi1221@gmail.com>
2025-05-07 11:53:09 -0300

    Adicionando página home e barra de ferramentas com rotas pelo sistema.

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,13 @@ import './index.css';
 import App from './App.jsx';
 import { Login } from './pages/Login.jsx';
 import Register from './pages/Register.jsx';
+import Home from './pages/Home.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<App />} />
         <Route path="/register" element={<Register />} />
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,60 +2,64 @@ import React from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import styles from './css/Register.module.css';
+import MyHeader from '../components/MyHeader';
+import { Container } from '@mui/material';
 
 const Register = () => {
   const { register, handleSubmit, formState: { errors } } = useForm();
   const navigate = useNavigate();
 
   const onSubmit = async (data) => {
-    // Simulação de registro (você pode substituir por uma chamada de API real)
     console.log('Dados registrados:', data);
     alert('Registro realizado com sucesso!');
-    navigate('/login'); // Redireciona para a página de login após o regist
+    navigate('/login');
     
   };
 
   return (
-    <div className={styles.container}>
-     <h1 className={styles.title}>Registro</h1>
-      <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
-        <label htmlFor="name" className={styles.label}>
-          Username
-          <input
-            {...register("name", { required: "Nome é obrigatório" })}
-            className={styles.input}
-          />
-          {errors.name && <span>{errors.name.message}</span>}
-        </label>
+    <Container>
+      <MyHeader />
+      <div className={styles.container}>
+      <h1 className={styles.title}>Registro</h1>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.form}>
+          <label htmlFor="name" className={styles.label}>
+            Username
+            <input
+              {...register("name", { required: "Nome é obrigatório" })}
+              className={styles.input}
+            />
+            {errors.name && <span>{errors.name.message}</span>}
+          </label>
 
-        <label htmlFor="email" className={styles.label}>
-          Email
-          <input
-            {...register("email", {
-              required: "Email é obrigatório",
-              
-            })}
-            className={styles.input}
-          />
-          {errors.email && <span>{errors.email.message}</span>}
-        </label>
+          <label htmlFor="email" className={styles.label}>
+            Email
+            <input
+              {...register("email", {
+                required: "Email é obrigatório",
+                
+              })}
+              className={styles.input}
+            />
+            {errors.email && <span>{errors.email.message}</span>}
+          </label>
 
-        <label htmlFor="password" className={styles.label}>
-          Password
-          <input
-            {...register("password", { required: "Senha é obrigatória" })}
-            type="password"
-            className={styles.input}
-          />
-          {errors.password && <span>{errors.password.message}</span>}
-        </label>
+          <label htmlFor="password" className={styles.label}>
+            Password
+            <input
+              {...register("password", { required: "Senha é obrigatória" })}
+              type="password"
+              className={styles.input}
+            />
+            {errors.password && <span>{errors.password.message}</span>}
+          </label>
 
-        <button type="submit" className={styles.button}>Registrar</button>
-        <p onClick={() => navigate('/Login')} className={styles.register}>
-          Já possui conta? Faça login.
-        </p>
-      </form>
-    </div>
+          <button type="submit" className={styles.button}>Registrar</button>
+          <p onClick={() => navigate('/Login')} className={styles.register}>
+            Já possui conta? Faça login.
+          </p>
+        </form>
+      </div>
+    </Container>
   );
 };
 
